d747bf229600080a80701b27977d2bf9f3aba929
Rodney Barnes <rbarnes@reliqhealth.com>
2021-05-24 19:40:34 -0400

    Add hello_cargo program

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-cb4a62b31832b48c.d
@@ -0,0 +1,5 @@
+c:\src\rust\projects\hello_cargo\target\rls\debug\deps\hello_cargo-cb4a62b31832b48c.rmeta: src\main.rs
+
+c:\src\rust\projects\hello_cargo\target\rls\debug\deps\hello_cargo-cb4a62b31832b48c.d: src\main.rs
+
+src\main.rs:
